69291b4bc624e2d547ce1070f4fd33bb2d3163ac
Srishti <srishtibansal505@gmail.com>
2025-04-07 22:48:39 +0530

    Solution to Count Distinct Substrings

--- Count Distinct Substrings@@67877c59ddaeab38c8ad25b0/code.c
+++ Count Distinct Substrings@@67877c59ddaeab38c8ad25b0/code.c
@@ -1,4 +1,4 @@
-// Your code here...#include <stdio.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
@@ -16,3 +16,34 @@ int main() {
     int len = strlen(str);
     char *substrings[MAX * MAX];
     int count = 0;
+
+    // Generate all substrings
+    for (int i = 0; i < len; i++) {
+        for (int j = 1; j <= len - i; j++) {
+            char *temp = (char *)malloc(j + 1);
+            strncpy(temp, str + i, j);
+            temp[j] = '\0';
+            substrings[count++] = temp;
+        }
+    }
+
+    // Sort all substrings
+    qsort(substrings, count, sizeof(char *), cmpfunc);
+
+    // Count distinct substrings
+    int uniqueCount = 1;  // First one is always unique
+    for (int i = 1; i < count; i++) {
+        if (strcmp(substrings[i], substrings[i - 1]) != 0) {
+            uniqueCount++;
+        }
+    }
+
+    printf("%d\n", uniqueCount);
+
+    // Free memory
+    for (int i = 0; i < count; i++) {
+        free(substrings[i]);
+    }
+
+    return 0;
+}
