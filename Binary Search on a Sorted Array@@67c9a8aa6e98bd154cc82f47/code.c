73f23155e2dce5c67ef7c7f6e8d8f93df768444d
Srishti <srishtibansal505@gmail.com>
2025-04-07 21:56:32 +0530

    Solution to Binary Search on a Sorted Array

---
+++ Binary Search on a Sorted Array@@67c9a8aa6e98bd154cc82f47/code.c
@@ -0,0 +1,41 @@
+// Your code here...
+// Your code here..
+#include <stdio.h>
+
+void moveZerosToEnd(int arr[], int n) {
+    int index = 0; // Index to place non-zero elements
+
+    // Traverse the array and move non-zero elements forward
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != 0) {
+            arr[index++] = arr[i];
+        }
+    }
+
+    // Fill the remaining positions with zeros
+    while (index < n) {
+        arr[index++] = 0;
+    }
+}
+
+int main() {
+    int n;
+
+   
+    scanf("%d", &n);
+
+    int arr[n];
+ 
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    moveZerosToEnd(arr, n);
+
+    
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    printf("\n");
+    return 0;
+}
